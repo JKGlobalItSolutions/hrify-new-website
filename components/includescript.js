063a674c001d1baca7e1879fa07ba78418f700c6
SUDHARSAN <kkqqq77288@gmail.com>
2025-11-20 17:43:38 +0530

    updates

--- components/includescript.js
+++ components/includescript.js
@@ -1,10 +1,10 @@
     document.addEventListener("DOMContentLoaded", function () {
         // Use absolute path from root directory
-        fetch("/components/header.html")
+        fetch("./components/header.html")
             .then(response => response.text())
             .then(data => document.getElementById("navbar-container").innerHTML = data);
 
-        fetch("/components/footer.html")
+        fetch("./components/footer.html")
             .then(response => response.text())
             .then(data => document.getElementById("footer").innerHTML = data);
     });
